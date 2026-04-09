5b6c52340816ad2c5f9578bc077612f9b103f66d
Jairo <jairo15102001@gmail.com>
2026-04-09 08:56:32 -0500

    Menu Transporte

---
+++ Transporte.java
@@ -0,0 +1,17 @@
+//meidnate la edad calcular el boleto del avion, menor de edad mitad de precio
+//<18 50%, >=64 75%
+
+//calcular valor del transporte terrestres por km
+//<= 100 km $5 <=200 %7,50, >=200 $10
+import java.util.Scanner;
+
+public class Transporte {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int menu = 0;
+         do {
+
+
+         }while (menu !=3);
+    }
+}
