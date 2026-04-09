4c800c6cb07a9145883e5e53b4ca9d5434e95888
moises <moi8332xd@gmail.com>
2026-04-09 09:26:29 -0500

    calcular kilometros

--- Transporte.java
+++ Transporte.java
@@ -4,14 +4,30 @@
 //calcular valor del transporte terrestres por km
 //<= 100 km $5 <=200 %7,50, >=200 $10
 import java.util.Scanner;
-
+import javax.swing.JOptionPane;
 public class Transporte {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int menu = 0;
-         do {
+        Double cantKM;
+        cantKM = Double.parseDouble(JOptionPane.showInputDialog("ingrese la cantidad de kilometros que deseas viajar"));
+
+        if (cantKM <=100) {
+            JOptionPane.showMessageDialog(null, "el valor del viaje es $5");
+            
+        }else if (cantKM <=200) {
+            JOptionPane.showMessageDialog(null, "el valor del viaje es $7.5");
+            
+        }else if (cantKM>=200) {
+            JOptionPane.showMessageDialog(null, "el valor del viaje es $10");
+        }
+
+       
+
+        
 
 
-         }while (menu !=3);
+
+       
     }
 }
